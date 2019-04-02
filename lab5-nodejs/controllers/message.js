2933c08adffe0120a1363c2ea2da530e69ac0e95
ApacheHelikopter <pietersruben@hotmail.com>
2019-04-02 20:46:03 +0200

    DONE: Added all routes which are all working WITHOUT mongoDB TODO: Complete lab5 by connecting routes to mongoDB and put application on heroku

--- lab5-nodejs/controllers/message.js
+++ lab5-nodejs/controllers/message.js
@@ -1,8 +1,49 @@
-let get = (req, res) =>{
+/* GET: /api/v1/messages */
+let get = (req, res, next) =>{
     res.json({
         "status": "success",
-        "messages": "GET messages"
-    });
+        "message": "GETTING messages"
+    })
 }
 
+/* GET: /api/v1/messages/:id */
+let getId = (req, res, next) => {
+    let messageId = req.params.id;
+    res.json({
+        "status": "success",
+        "message": "GETTING message with ID: " + messageId
+    })
+}
+
+/* POST: /api/v1/messages */
+let post = (req, res, next) => {
+    res.json({
+        "status": "success",
+        "message": "POSTING a new message for user Pikachu"
+    })
+}
+
+/* PUT: /api/v1/messages/:id */
+let put = (req, res, next) => {
+    let messageId = req.params.id;
+    res.json({
+        "status": "success",
+        "message": "UPDATING a message with ID: " + messageId
+    })
+}
+
+/* DELETE: /api/v1/messages/:id */
+let del = (req, res, next) => {
+    let messageId = req.params.id;
+    res.json({
+        "status": "success",
+        "message": "DELETING a message with ID: " + messageId
+    })
+}
+
+
 module.exports.get = get;
+module.exports.getId = getId;
+module.exports.post = post;
+module.exports.put = put;
+module.exports.del = del;
--- lab5-nodejs/routes/index.js
+++ lab5-nodejs/routes/index.js
@@ -11,4 +11,16 @@ router.get('/', function(req, res, next) {
 /* GET: /api/v1/messages */
 router.get('/api/v1/messages' , messageController.get);
 
+/* GET: /api/v1/messages/:id */
+router.get('/api/v1/messages/:id', messageController.getId);
+
+/* POST: /api/v1/messages */
+router.post('/api/v1/messages', messageController.post);
+
+/* PUT: /api/v1/messages/:id */
+router.put('/api/v1/messages/:id', messageController.put);
+
+/* DELETE: /api/v1/messages/:id */
+router.delete('/api/v1/messages/:id', messageController.del);
+
 module.exports = router;
