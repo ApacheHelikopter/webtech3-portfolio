c067bec953cb6bad11ca23c792f38b1413cca37b
ApacheHelikopter <pietersruben@hotmail.com>
2019-04-05 11:31:53 +0200

    DONE: Added a question with 2 choices. Both choices are being read in the browser and sent through Primus and can be read out in console. TODO: Add custom CSS. Add more functionalities to make the responses visible.

--- lab5-nodejs/routes/index.js
+++ lab5-nodejs/routes/index.js
@@ -1,30 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var cors = require('cors');
+
 
 var messageController = require('../controllers/message.js');
 
 /* GET: / */
-router.get('/', cors(),function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'IMD CHAT' });
 });
 
 /* GET: /api/v1/messages */
-router.get('/api/v1/messages' , cors(),messageController.get);
+router.get('/api/v1/messages' , messageController.get);
 
 /* GET: /api/v1/messages/:id */
-router.get('/api/v1/messages/:id', cors(),messageController.getId);
+router.get('/api/v1/messages/:id', messageController.getId);
 
 /* POST: /api/v1/messages */
-router.post('/api/v1/messages', cors(),messageController.post);
+router.post('/api/v1/messages', messageController.post);
 
 /* PUT: /api/v1/messages/:id */
-router.put('/api/v1/messages/:id', cors(),messageController.put);
+router.put('/api/v1/messages/:id', messageController.put);
 
 /* DELETE: /api/v1/messages/:id */
-router.delete('/api/v1/messages/:id', cors(),messageController.del);
+router.delete('/api/v1/messages/:id', messageController.del);
 
 /* GET: /api/v1/messages?user=username */
-router.get('/api/v1/messages' , cors(),messageController.get);
+router.get('/api/v1/messages' , messageController.get);
 
 module.exports = router;
